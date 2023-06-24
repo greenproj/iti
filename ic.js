23febe031757e28b92ad67bf896ea47b7f4514ef
greenproj <116536705+greenproj@users.noreply.github.com>
2023-06-24 19:19:07 +0530

    script.js

--- ic.js
+++ ic.js
@@ -1,26 +1,49 @@
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  
-  var fileInput = document.getElementById('fileInput');
-  var file = fileInput.files[0];
-  
-  var formData = new FormData();
-  formData.append('image', file);
-  
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'compress.php', true);
-  
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText);
-      if (response.success) {
-        var output = document.getElementById('output');
-        output.innerHTML = '<a href="' + response.compressedURL + '" download>Download Compressed Image</a>';
-      } else {
-        alert('Error compressing image: ' + response.error);
-      }
-    }
-  };
-  
-  xhr.send(formData);
+window.addEventListener('DOMContentLoaded', () => {
+    const inputFile = document.getElementById('inputFile');
+    const compressButton = document.getElementById('compressButton');
+    const originalImage = document.getElementById('originalImage');
+    const compressedImage = document.getElementById('compressedImage');
+    const downloadLink = document.getElementById('downloadLink');
+
+    inputFile.addEventListener('change', () => {
+        const file = inputFile.files[0];
+        const reader = new FileReader();
+
+        reader.addEventListener('load', () => {
+            originalImage.src = reader.result;
+        });
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    });
+
+    compressButton.addEventListener('click', () => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        const maxWidth = 800; // Maximum width for the compressed image
+
+        const image = new Image();
+        image.src = originalImage.src;
+
+        image.addEventListener('load', () => {
+            let width = image.width;
+            let height = image.height;
+
+            if (width > maxWidth) {
+                height *= maxWidth / width;
+                width = maxWidth;
+            }
+
+            canvas.width = width;
+            canvas.height = height;
+            ctx.drawImage(image, 0, 0, width, height);
+
+            const compressedDataURL = canvas.toDataURL('image/jpeg', 0.7); // Adjust compression quality if needed
+
+            compressedImage.src = compressedDataURL;
+            downloadLink.href = compressedDataURL;
+            downloadLink.style.display = 'inline';
+        });
+    });
 });
